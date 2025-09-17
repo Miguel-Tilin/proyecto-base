c0b5f6179828d78e4f86f47bb9b1f800baf20645
Migue Trujillo <juanmigueltrujillosanchez5@gmail.com>
2025-09-17 11:12:07 -0600

    Version 2

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -7,23 +7,42 @@ class Operaciones:
     def leerNumeros(self):
         while True:
             try:
-                self.num1 = int(input("Número 1:"))
+                self.num1 = int(input("Número 1: "))
                 break
             except Exception:
                 print("Número inválido")
-                continue
         while True:
             try:
-                self.num2 = int(input("Número 2:"))
+                self.num2 = int(input("Número 2: "))
                 break
             except Exception:
-                print("Número inválido")
-                continue    
+                print("Número inválido")    
     
     def sumar(self):
-        self.resultado = "La suma de " + str(self.num1) + " + " + str(self.num2) + " es igula a " + str(self.num1 + self.num2)
+        self.resultado = "La suma de " + str(self.num1) + " + " + str(self.num2) + " es igual a " + str(self.num1 + self.num2)
+    
+    def restar(self):
+        self.resultado = "La resta de " + str(self.num1) + " - " + str(self.num2) + " es igual a " + str(self.num1 - self.num2)
+    
+    def multiplicacion(self):
+        self.resultado = "La multiplicación de " + str(self.num1) + " * " + str(self.num2) + " es igual a " + str(self.num1 * self.num2)
+
+    def division(self):
+        if self.num2 != 0:
+            self.resultado = "La división de " + str(self.num1) + " / " + str(self.num2) + " es igual a " + str(self.num1 / self.num2)
+        else:
+            self.resultado = "Error: no se puede dividir entre 0"
+
+    def resto(self):
+        if self.num2 != 0:
+            self.resultado = "El resto de " + str(self.num1) + " % " + str(self.num2) + " es igual a " + str(self.num1 % self.num2)
+        else:
+            self.resultado = "Error: no se puede obtener el resto con divisor 0"
     
     def mostrarResultado(self):
         print(self.resultado)
-        
+
+
+
+
         
--- main.py
+++ main.py
@@ -2,9 +2,36 @@ from clases.operaciones import Operaciones
 
 def main():
     op = Operaciones()
-    op.leerNumeros()
-    op.sumar()
-    op.mostrarResultado()
+    while True:
+        print("\n--- CALCULADORA ---")
+        print("1. Sumar")
+        print("2. Restar")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("5. Resto (módulo)")
+        print("6. Salir")
+        
+        opcion = input("Elige una opción: ")
+
+        if opcion == "6":
+            print("Saliendo de la calculadora...")
+            break
+        elif opcion in ["1", "2", "3", "4", "5"]:
+            op.leerNumeros()
+            if opcion == "1":
+                op.sumar()
+            elif opcion == "2":
+                op.restar()
+            elif opcion == "3":
+                op.multiplicacion()
+            elif opcion == "4":
+                op.division()
+            elif opcion == "5":
+                op.resto()
+            op.mostrarResultado()
+        else:
+            print("Opción inválida, intenta de nuevo.")
+
     
 if __name__ == "__main__":
     main()
